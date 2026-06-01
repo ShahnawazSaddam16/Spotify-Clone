69353387652159e26af0c9741d9750fb5f8f3c35
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-06-02 00:13:15 +0500

    creating topbar

---
+++ components/HomeComponents/TopBar.js
@@ -0,0 +1,13 @@
+import {View, Text} from "react-native";
+
+export default function TopBar(){
+    return(
+        <>
+        <View className="">
+            <Text className="text-white">
+                
+            </Text>
+        </View>
+        </>
+    )
+ }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,13 +2,14 @@ import { View, Text } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { PRIMARY_COLOR } from "../theme/theme";
 import BottomBar from "../components/BottomBar";
+import TopBar from "../components/HomeComponents/TopBar";
 
 export default function HomeScreen() {
   return (
-    <View
-      className="flex-1"
-      style={{ backgroundColor: PRIMARY_COLOR }}
-    >
+    <View style={{ flex: 1, backgroundColor: PRIMARY_COLOR }}>
+      
+      <TopBar />
+
       <View className="flex-1 justify-center items-center">
         <Text className="text-white">HomeScreen</Text>
       </View>
