20fffcee716ac62339dab006ed2a14bcdd74f667
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-06-06 16:38:20 +0500

    adding discover new in search screen

--- components/SearchComponents/DiscoverNew.js
+++ components/SearchComponents/DiscoverNew.js
@@ -1,13 +1,58 @@
-import { Text, View } from "react-native";
+import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
 
+export default function DiscoverNew() {
+  const playlists = [
+    {
+      image: require("../../assets/albums/image10.jpg"),
+      tag: "#falak",
+    },
+    {
+      image: require("../../assets/albums/image11.jpg"),
+      tag: "#classic pop",
+    },
+    {
+      image: require("../../assets/albums/image12.jpg"),
+      tag: "#sad",
+    },
+    {
+      image: require("../../assets/albums/image13.jpg"),
+      tag: "#love",
+    },
+  ];
 
-export default function DiscoverNew(){
-    return(
-        <>
-        <View className="flex-1 items-start mt-10 ml-5">
-            <Text className="text-[#FFFFFF] font-extrabold text-[22px]">
-            Discover Something New</Text>
-        </View>
-        </>
-    )
+  return (
+    <View className="mt-8">
+      <Text className="text-white text-[22px] ml-3 font-extrabold mb-5">
+        Discover something new
+      </Text>
+
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingRight: 20 }}
+      >
+        {playlists.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            activeOpacity={0.9}
+            className="mr-4"
+          >
+            <View className="w-[150px] h-[210px] rounded-2xl overflow-hidden">
+              <Image
+                source={item.image}
+                className="w-full h-full"
+                resizeMode="cover"
+              />
+
+              <View className="absolute bottom-0 left-0 right-0 px-3 py-3">
+                <Text className="text-white text-[15px] font-extrabold">
+                  {item.tag}
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    </View>
+  );
 }
