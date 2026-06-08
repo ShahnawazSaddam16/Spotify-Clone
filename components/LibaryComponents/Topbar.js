82776d4d0a5168ad9dd5e696b8c5ddec6b58cfe6
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-06-09 00:25:50 +0500

    adding topbar componet in libary scren

---
+++ components/LibaryComponents/Topbar.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+export default function TopBar(){
+    return(
+        <>
+        <View>
+            <Text className="text-[#FFFFFF]">Hello</Text>
+        </View>
+        </>
+    )
+}
--- screens/LibraryScreen.js
+++ screens/LibraryScreen.js
@@ -2,11 +2,14 @@ import {View, Text} from "react-native";
 import {PRIMARY_COLOR} from "../theme/theme";
 import { StatusBar } from "react-native";
 import BottomBar from "../components/BottomBar";
+import TopBar from "../components/LibaryComponents/Topbar";
 
 export default function LibraryScreen(){
     return(
         <>
         <View style={{flex:1, backgroundColor: PRIMARY_COLOR}}>
+            <TopBar />
+
             <View className="flex-1 justify-center items-center">
                 <Text className="text-white">LibaryScreen</Text>
             </View>
